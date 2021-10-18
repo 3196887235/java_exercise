f90363bdd421bc520098db7c929c88d89043bb9f
xw <3196887235@qq.com>
2021-10-18 13:20:21 +0800

    Create IdentfierTest.java

---
+++ 标识符/day03/IdentfierTest.java
@@ -0,0 +1,24 @@
+/*
+标识符的使用
+1.标识符：凡是自己可以起名字的地方都叫标识符
+比如：类名，变量名，方法名，接口名，包名...
+
+2.重点掌握：标识符命名规则
+由26个英文字母大小写，0到9，或_或$
+数字不可以开头
+
+
+
+
+
+*/
+
+
+
+class  IdentifierTest{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
+class Hello1_${
+}
