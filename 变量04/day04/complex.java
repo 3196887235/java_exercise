bb9479e321317324e6a827257b45d029fb3da2fe
xw <3196887235@qq.com>
2021-10-18 13:20:01 +0800

    Create complex.java

---
+++ 变量04/day04/complex.java
@@ -0,0 +1,30 @@
+#定义复数这一数据结构
+class  complex{
+	double realpart;
+	float imagpart;
+}
+public class Complex
+{
+	void assign(float real,float imag){
+
+
+		
+	}
+	void add(){
+		
+	}
+	void minus(){
+		
+	}
+	void multiply(){
+		
+	}
+	void divide(){
+		
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+	}
+}
