95d49e4f225e09fbb9186d6143201ac2dc4d99a3
xw <3196887235@qq.com>
2021-10-18 13:20:05 +0800

    Create MainTest.java

---
+++ 变量04/day04/MainTest.java
@@ -0,0 +1,11 @@
+/*
+public static void main(String[] args) {}
+可以被代替为public static void main(String\u005B\u005D args) {}
+变为其他不行
+*/
+class MainTest {
+	public static void main(String\u005B\u005D args) {
+		int i,j;
+		System.out.println("Hello World!");
+	}
+}
